29810a0e09875154554b13211c80b72be54b528d
Kenan Casey <kenancasey@gmail.com>
2018-02-22 09:05:28 -0600

    Add SampleData

--- StarWars/Program.cs
+++ StarWars/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace StarWars
 {
@@ -6,65 +7,8 @@ namespace StarWars
     {
         static void Main(string[] args)
         {
+            var characters = SampleData.Characters;
 
-            Character luke = new Character() 
-            { 
-                Name = "Luke Skywalker",
-                Species = "Human",
-                MidichlorianCount = 15000
-            };
-
-            Character ani = new Character()
-            {
-                Name = "Anakin Skywalker",
-                Species = "Human",
-                MidichlorianCount = 27000
-            };
-
-            Character leia = new Character()
-            {
-                Name = "Leia Organa",
-                Species = "Human",
-                MidichlorianCount = 15000
-            };
-
-            Character han = new Character()
-            {
-                Name = "Han Solo",
-                Species = "Human",
-                MidichlorianCount = 0
-            
-            };
-
-            Character yoda = new Character()
-            {
-                Name = "Yoda",
-                Species = "Unknown",
-                MidichlorianCount = 17000
-            };
-
-
-            var empireStikesBack = new Movie()
-            {
-                Name="The Empire Strikes Back",
-                ReleaseDate = new DateTime(1980,5,21)
-            };
-
-            empireStikesBack.Characters.Add(luke);
-            empireStikesBack.Characters.Add(ani);
-            empireStikesBack.Characters.Add(leia);
-            empireStikesBack.Characters.Add(han);
-            empireStikesBack.Characters.Add(yoda);
-
-            Console.WriteLine(empireStikesBack);
-
-            empireStikesBack.Characters.Sort();
-
-            Console.WriteLine(empireStikesBack);
-
-            if ( ani > luke) {
-                //do something 
-            }
 
         }
     }
---
+++ StarWars/SampleData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StarWars
+{
+    public class SampleData
+    {
+        public SampleData()
+        {
+        }
+
+        public static List<Character> Characters
+        {
+            get
+            {
+                var chars = new List<Character>()
+                {
+                    new Character()
+                    {
+                        Name = "Luke Skywalker",
+                        Species = "Human",
+                        MidichlorianCount = 15000
+                    },
+
+                    new Character()
+                    {
+                        Name = "Anakin Skywalker",
+                        Species = "Human",
+                        MidichlorianCount = 27000
+                    },
+
+                     new Character()
+                    {
+                        Name = "Leia Organa",
+                        Species = "Human",
+                        MidichlorianCount = 15000
+                    },
+
+                     new Character()
+                    {
+                        Name = "Han Solo",
+                        Species = "Human",
+                        MidichlorianCount = 0
+
+                    },
+
+                     new Character()
+                    {
+                        Name = "Yoda",
+                        Species = "Unknown",
+                        MidichlorianCount = 17000
+                    }
+                };
+
+                return chars;
+            }
+        }
+    }
+}
